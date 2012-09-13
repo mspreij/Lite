f63140d9e6f7fc0ea976e7639c044d4dca842350
Maarten Spreij <mspreij@gmail.com>
2012-09-13 20:37:31 +0200

    auth thinger

--- inc/lite.inc.php
+++ inc/lite.inc.php
@@ -36,27 +36,7 @@ ob_start();
 require_once './inc/functions.inc.php';
 $cookie_name = 'xizzy';
 
-// Check cookie
-if (! isset($_COOKIE['nemo'])) {
-//	die("Access Denied (c)");
-}
-// Check IP
-$valid_ips = array('192.168.1.777');
-if (! in_array($_SERVER['REMOTE_ADDR'], $valid_ips)) {
-	die("Access Denied ({$_SERVER['REMOTE_ADDR']})");
-}
-
-// Fucking fastcgi...
-// @list($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']) = explode(':' , base64_decode(substr(@$_SERVER['HTTP_AUTHORIZATION'], 6)));
-
-// Basic auth
-$u = @$_SERVER['PHP_AUTH_USER'];
-$p = @$_SERVER['PHP_AUTH_PW'];
-if    ((md5($u) != 'hash string') || (md5($p) != 'nother hash string')) {
-	header('WWW-Authenticate: Basic realm="LiteAdmin"');
-	header('HTTP/1.1 401 Unauthorized');
-	die("Access Denied (u/p)");
-}
+require_once './inc/auth.inc.php';
 
 $me = basename($_SERVER['SCRIPT_FILENAME']);
 $messages = array();
